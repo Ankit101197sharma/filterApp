f66c932dd0bda4b0ef7c4ed104e4ceeed5ee21e1
ankit101197sharma <ankit101197sharma@gmail.com>
2022-09-08 13:49:35 +0530

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchquery, setSearchquery] = useState(false)
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      setPosts(response.data);
+      setLoading(false);
+    };
+    loadPosts();
+  }, []);
+   
+ let res = posts.map((item) => (
+  <div className="col-md-4 m-5 justify-content-center " style={{border:"1px solid #ccc", }}>
+    <h5 key={item.id}> {item.title} </h5>
+    <p> {item.body} </p>
+  </div>
+))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>
+          <h2>Search Filter</h2>
+          <div className="">
+          <input
+            style={{ width: "30%", height: "25px" }}
+            type="text"
+            value={searchTitle}
+            onChange={(e) => setSearchTitle(e.target.value)}
+            placeholder="search..."
+          />
+          <button className="btn btn-primary m-2"  type="search"  onClick={()=>setSearchquery(true)} > Search</button>
+
+          </div>
+          
+          {loading ? (
+            <h4>Loading ...</h4>
+          ) : (
+            (searchquery) ?  posts
+            .filter((value) => (
+                value.title.toLowerCase().includes(searchTitle.toLowerCase())
+              )
+            ).res : res
+           
+              
+          )}
+        </div>
+      </div>
     </div>
   );
 }
